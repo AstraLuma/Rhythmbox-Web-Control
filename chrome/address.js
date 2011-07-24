027f1a7529d3e729c47094a46106378231b1173e
James Bliss <james.bliss@astro73.com>
2011-07-24 12:35:17 -0400

    * Added documentation * Added support for offline/online events, to reduce polling.

--- chrome/address.js
+++ chrome/address.js
@@ -1,3 +1,8 @@
+/**
+ * Acts as a poor man's configuration. Takes a URL path (everthing from the 
+ * first/third slash on, "/foo/bar") and returns the absolute URL to pass to eg 
+ * XHR.
+ */
 function getrburl(page) {
 	return "http://192.168.10.111:5678"+page;
 }
